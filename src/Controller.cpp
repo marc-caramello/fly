67d254c5a71177080311411cf2c86b002344bc23
marc-caramello <marc.caramello@gmail.com>
2023-11-08 03:57:24 -0500

    I cleaned things up, exe works when moved into the fly folder

---
+++ src/Controller.cpp
@@ -0,0 +1,39 @@
+#include "Controller.h"
+#include "Log.h"
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+
+namespace fly
+{
+
+Controller::Controller(sf::Window& window)  :
+    m_window(window)
+{
+}
+
+
+void Controller::takeInput()
+{
+    // some shorthands..
+    using Key       = sf::Keyboard;
+    using IsKeyPressedFunction = bool (*)(sf::Keyboard::Key);
+    IsKeyPressedFunction isPressed = sf::Keyboard::isKeyPressed;
+
+    if (isPressed(Key::W) || isPressed(Key::Up))
+        m_callbacks[ElevatorDown]();
+    if (isPressed(Key::S) || isPressed(Key::Down))
+        m_callbacks[ElevatorUp]();
+
+    if (isPressed(Key::A) || isPressed(Key::Left))
+        m_callbacks[RollLeft]();
+    if (isPressed(Key::D) || isPressed(Key::Right))
+        m_callbacks[RollRight]();
+
+    if (isPressed(Key::Space))
+        m_callbacks[ThrustUp]();
+    if (isPressed(Key::LShift))
+        m_callbacks[ThrustDown]();
+
+}
+
+}
